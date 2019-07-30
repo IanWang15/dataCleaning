df60a55d8e4ba9d024971fcfdf76a306a921bb61
YiWang <yiwang@client-10-229-164-148.tamulink.tamu.edu>
2019-07-29 22:21:00 -0500

    Update parsingCSV.py
    update from python2 to python3

--- parsingCSV.py
+++ parsingCSV.py
@@ -2,6 +2,7 @@
 # where the key is the header title of the field, and the value is the value of that field in the row.
 # The function parse_file should return a list of dictionaries,
 # each data line in the file being a single list entry.
+# update from python2 to python3
 import os
 
 DATADIR = "./"
@@ -11,13 +12,13 @@ def parse_file(datafile):
     data = []
     with open(datafile, "r") as f:
 		header = f.readline().split(",")
-#		print header
+#		print(header)
 # ['Title', 'Released', 'Label', 'UK Chart Position', 'US Chart Position', 'BPI Certification', 'RIAA Certification\n']
-#		print header[6].strip()
+#		print(header[6].strip())
 		counter = 0
-#		print f.readline()
+#		print(f.readline())
 		for line in f:
-#		    print line
+#		    print(line)
 		    # ex. Please Please Me,22 March 1963,Parlophone(UK),1,-,Gold,Platinum
 		    if counter == 10: # process 10 lines only in this practise, because the following lines have different format
 		        break
@@ -25,12 +26,12 @@ def parse_file(datafile):
 		    entry = {} # create an empty dictionary
 		    
 		    for i,value in enumerate(fields):
-#		        print fields
+#		        print(fields)
 		        # ex. ['Please Please Me', '22 March 1963', 'Parlophone(UK)', '1', '-', 'Gold', 'Platinum\n']
-#		        print i, value
+#		        print(i, value)
 		        # ex. 0 With the Beatles
-#		        print value.strip()
-#		        print header[i].strip()
+#		        print(value.strip())
+#		        print(header[i].strip())
 		        entry[header[i].strip()] = value.strip()
 		        # dict[key] = list
 		        # add a list into the dictionary
@@ -52,7 +53,7 @@ def test():
     firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
     tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
 
-#    print d[0]
+#    print(d[0])
     assert d[0] == firstline
     assert d[9] == tenthline
 
